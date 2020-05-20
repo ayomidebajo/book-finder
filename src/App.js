894224e43140c19bfeb23d5c309b528eac105cdb
https://github.com/ayomidebajo <oluwashinabajo@gmail.com>
2020-05-20 17:46:31 +0100

    Corrections

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
 
   const apiKey = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
-  let ClientId;
+  let clientId;
 
   if (process.env.NODE_ENV !== "production") {
     clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
